c3a084e87da7521e57c322f823daf58c1d3f0cd0
Bernd Porr <bernd@glasgowneuro.tech>
2019-12-03 10:26:07 +0000

    Removed the old cocoa pods file from a previous Project

--- Pods/Target Support Files/Pods-Tinnitus Tailor-Tinnitus TailorUITests/Pods-Tinnitus Tailor-Tinnitus TailorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tinnitus_Tailor_Tinnitus_TailorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Tinnitus_Tailor_Tinnitus_TailorUITests
-@end
--- Pods/Target Support Files/Pods-Tinnitus Tailor/Pods-Tinnitus Tailor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tinnitus_Tailor : NSObject
-@end
-@implementation PodsDummy_Pods_Tinnitus_Tailor
-@end
